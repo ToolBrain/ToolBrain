23dd01bb6acdd5783ee530a4f3740ed5810b2f10
khue <luumsk@gmail.com>
2025-08-05 20:41:58 +0700

    add main logic for the train function

--- grpo.py
+++ grpo.py
@@ -1,9 +1,42 @@
+import random
+from copy import deepcopy
+
+
+def copy_model(model):
+    return model.copy() if hasattr(model, 'copy') else deepcopy(model)
+
+def sample_batch(dataset, batch_size=4):
+    return random.sample(dataset, batch_size)
+
+def compute_advantanges(rewards):
+    pass
+
+def grpo_loss():
+    pass
+
+def update_policy():
+    pass
+
+
 def train_grpo(
         initial_policy,
         traces,
         reward_function,
         config,
-): pass
+):
+    # pi_theta is the updated policy model
+    pi_theta = copy_model(initial_policy)
+
+    for _ in range(config["I"]):
+        rewards = [reward_function(trace) for trace in traces]
+        advantages = compute_advantanges(rewards)
+
+        for _ in range(config["mu"]):
+            loss = grpo_loss(rewards, advantages)
+            pi_theta = update_policy(loss)
+
+    return pi_theta
+
 
 if __name__ == "__main__":
     initial_policy = None
