0c60684487484573b343145f1dce5baebcc0c5af
khue <luumsk@gmail.com>
2025-08-07 01:36:03 +0700

    refactor grpo

--- grpo.py
+++ grpo.py
@@ -1,42 +1,8 @@
 # Paper DeepSeekMath: https://arxiv.org/pdf/2402.03300
 
 import torch
-from wrapper import CodeAgentWrapper
 from grpo_utils import *
-
-def grpo_loss(
-        pi_theta_log_probs: torch.Tensor,
-        pi_ref_log_probs: torch.Tensor,
-        advantages: torch.Tensor,
-        epsilon: float,
-        beta: float,
-        completion_mask: torch.Tensor
-) -> torch.Tensor:
-    """
-    Computes the GRPO loss function as defined in Equation (3) of the DeepSeekMath paper.
-    This implementation returns the negative of the original gain (objective) for optimization via gradient descent.
-    """
-
-    # Clipped surrogate gain
-    log_ratio = pi_theta_log_probs - pi_ref_log_probs  # log(pi_theta) - log(pi_ref) = log(pi_theta / pi_ref)
-    ratio = torch.exp(log_ratio)  # exp(log(pi_theta / pi_ref)) = pi_theta / pi_ref
-    unclipped = ratio * advantages  # shape: (T,)
-
-    clipped_ratio = torch.clamp(ratio, 1.0 - epsilon, 1.0 + epsilon)
-    clipped = clipped_ratio * advantages  # shape: (T,)
-
-    policy_gain = torch.min(unclipped, clipped)  # shape: (T,)
-
-    # KL divergence (Equation 4)
-    # Equation 4: (pi_ref / pi_theta) - log(pi_ref / pi_theta) - 1
-    log_kl_ratio = pi_ref_log_probs - pi_theta_log_probs  # log(pi_ref) - log(pi_theta) = log(pi_ref / pi_theta)
-    kl_ratio = torch.exp(log_kl_ratio)  # exp(log(pi_ref / pi_theta)) = pi_ref / pi_theta
-    kl_term = kl_ratio - log_kl_ratio - 1.0  # shape: (T,)
-
-    # Token-level loss
-    per_token_loss = -(policy_gain - beta * kl_term)  # shape: (T,)
-    loss = (per_token_loss * completion_mask).sum() / completion_mask.sum()
-    return loss # scalar loss value
+from losses import grpo_loss
 
 
 def update_policy(pi_theta, loss):
@@ -44,11 +10,11 @@ def update_policy(pi_theta, loss):
 
 
 def grpo_step(
-        initial_policy: CodeAgentWrapper,
+        initial_policy: Policy,
         trace: list[tuple[str, str]],
         reward_function: callable,
         config: dict
-) -> CodeAgentWrapper:
+) -> Policy:
     
     pi_theta = copy_model(initial_policy)
 
@@ -81,7 +47,8 @@ if __name__ == "__main__":
     def compute_reward(prompt, completion):
         return torch.rand(1, dtype=torch.float32).item()
 
-    initial_policy = CodeAgentWrapper("gpt2")
+    llm, tokenizer = get_llm_and_tokenizer_from_smolagent("gpt2")
+    initial_policy = Policy(llm=llm, tokenizer=tokenizer)
     trace = [
         ("What is 7 + 6?", "13"),
         ("Which number is greater: 12 or 9?", "12"),
--- grpo_utils.py
+++ grpo_utils.py
@@ -1,22 +1,32 @@
-import random
 from copy import deepcopy
 import torch
-from transformers import AutoTokenizer
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 
 
-def copy_model(model):
+def copy_model(model: nn.Module) -> nn.Module:
     return model.copy() if hasattr(model, 'copy') else deepcopy(model)
 
-def sample_batch(dataset, batch_size=4):
-    return random.sample(dataset, batch_size)
+def get_llm_and_tokenizer_from_smolagent(model_id: str):
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+    llm = AutoModelForCausalLM.from_pretrained(model_id)
+    return llm, tokenizer
 
-def compute_advantages(rewards):
+def compute_advantages(rewards: torch.Tensor) -> torch.Tensor:
     mean = rewards.mean()
     std = rewards.std(unbiased=False)
     normalized_r = (rewards - mean) / (std + 1e-8)
     return normalized_r
 
-def build_inputs(trace, tokenizer, reward_function):
+def build_inputs(
+    trace: list[tuple[str, str]],
+    tokenizer: PreTrainedTokenizerBase,
+    reward_function: callable
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
     """
     Args:
         trace: List of (prompt: str, completion: str) tuples.
@@ -59,20 +69,50 @@ def build_inputs(trace, tokenizer, reward_function):
 
     return input_ids, completion_mask, rewards, advantages
 
+def get_per_token_logps(logits: torch.Tensor, input_ids: torch.Tensor) -> torch.Tensor:
+    per_token_logps = [] # Use a loop to reduce memory peak.
+    for logits_row, input_ids_row in zip(logits, input_ids):
+        log_probs = logits_row.log_softmax(dim=-1)
+        token_log_prob = torch.gather(log_probs, dim=1, index=input_ids_row.unsqueeze(1)).squeeze(1)
+        per_token_logps.append(token_log_prob)
+    return torch.stack(per_token_logps)
+
+class Policy(nn.Module):
+    def __init__(self, llm, tokenizer) -> None:
+        super().__init__()
+        self.llm = llm
+        self.tokenizer = tokenizer
+
+    def get_log_probs(self, input_ids: torch.Tensor) -> torch.Tensor:
+        logits = self.llm(input_ids.unsqueeze(0)).logits  # (N, T, V)
+        #logits = logits[:, :-1, :]  # (N, T-1, V), exclude the last logit: it corresponds to the next token pred
+        
+        input_ids = input_ids.unsqueeze(0)  # (N, T), add batch dimension
+        #input_ids = input_ids[:, 1:]  # (N, T-1), exclude the first input ID since we don't have logits for it
+        
+        per_token_logps = get_per_token_logps(logits, input_ids).squeeze(0) # Shape: (T)
+        # per_token_logps = per_token_logps[completion_mask==1.0] # Only keep log probabilities for completion tokens
+        return per_token_logps
+
+
 if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    model_id = "gpt2"
+
+    llm, tokenizer = get_llm_and_tokenizer_from_smolagent(model_id=model_id)
+    policy_model = Policy(llm=llm, tokenizer=tokenizer)
 
     trace = [
-        ("What is the capital of France?", " Paris."),
-        ("2 + 2 equals", " 4."),
+        ("What is the capital of France?", "The capital of France is Paris."),
+        ("Who wrote 'Pride and Prejudice'?", "Jane Austen wrote 'Pride and Prejudice'."),
     ]
-
     input_ids, completion_mask, rewards, advantages = build_inputs(
         trace=trace,
         tokenizer=tokenizer,
-        reward_function=lambda p, c: torch.scalar_tensor(1.0)
-    )
+        reward_function=lambda p, c: 1.0)
+    log_probs = policy_model.get_log_probs(input_ids)
 
+    print("Log probabilities shape:", log_probs.shape)
+    print("Log probabilities:", log_probs)
     print("Input IDs:", input_ids)
     print("Shape of input_ids:", input_ids.shape)
     print("Completion Mask:", completion_mask)
